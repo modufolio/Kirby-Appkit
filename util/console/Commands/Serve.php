586613c58a111e3e7539fb21a6a5fd7c63ae4c0f
Maarten Thiebou <maarten@thinkyellow.nl>
2022-08-25 23:36:21 +0200

    Console added

---
+++ util/console/Commands/Serve.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Serve extends Command
+{
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'serve';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Serve the application on the PHP development server";
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+        $this->checkPhpVersion();
+
+        $this->info('Kirby development server started on http://localhost:8000');
+
+        $command =  PHP_BINARY . ' -S localhost:8000 -t public kirby/router.php';
+
+        passthru($command);
+    }
+
+    /**
+     * Check the current PHP version is >= 5.4.
+     *
+     * @return void
+     *
+     * @throws \Exception
+     */
+    protected function checkPhpVersion()
+    {
+        if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+            throw new \Exception('This PHP binary is not version 5.4 or greater.');
+        }
+    }
+}
