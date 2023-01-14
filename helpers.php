8d18b0a260a3426493b95347eadc04652b55f833
Maarten Thiebou <thiebou@gmail.com>
2023-01-15 00:58:20 +0100

    Check if helper functions exists

--- helpers.php
+++ helpers.php
@@ -4,30 +4,32 @@ use App\Core\Layout;
 use App\Core\Slots;
 use Kirby\Toolkit\A;
 
-function env(string $key)
-{
-    $data = parse_ini_file('.env', true);
-    return A::get($data, $key);
-}
-
-function layout($name = null, ?array $data = null)
-{
-    if (is_array($name) === true) {
-        $data = $name;
-        $name = null;
+if (! function_exists('env')) {
+    function env(string $key){
+        $data = parse_ini_file('.env', true);
+        return A::get($data, $key);
     }
-
-    Layout::start($name, $data);
 }
 
-function slot(?string $name = null)
-{
-    Slots::start($name);
+if (! function_exists('layout')) {
+    function layout($name = null, ?array $data = null)
+    {
+        if (is_array($name) === true) {
+            $data = $name;
+            $name = null;
+        }
+        Layout::start($name, $data);
+    }
 }
-
-function endslot()
-{
-    echo Slots::end();
+if (! function_exists('slot')) {
+    function slot(?string $name = null){
+        Slots::start($name);
+    }
+}
+if (! function_exists('slot')) {
+    function endslot(){
+        echo Slots::end();
+    }
 }
 
 
