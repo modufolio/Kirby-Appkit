0607b809f1ad221c49618d0e850fc36054f88c9a
Maarten Thiebou <thiebou@gmail.com>
2023-01-29 00:26:11 +0100

    add uuid to virtual pages example

--- config/pages.php
+++ config/pages.php
@@ -7,6 +7,7 @@ return [
         'template' => 'login',
         'model'    => 'login',
         'content'  => [
+            'uuid' =>  '874f489b-176e-4d7a-96d9-cd673ceef707',
             'title' => 'Login',
             'text'  => 'Some content here'
         ],
@@ -16,6 +17,7 @@ return [
         'template' => 'shop',
         'model'    => 'shop',
         'content'  => [
+            'uuid' =>  '12c4d5d2-4484-40bc-ac58-a2808c87f01a',
             'title' => 'Shop',
             'text'  => 'Some content here'
         ],
